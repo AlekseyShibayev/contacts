69825f674f3a7c012e3502c63749ae57c43fad19
AlekseyShibayev <mail9788615101@gmail.com>
2019-08-03 20:00:19 +0300

    refactoring for new age of education

--- src/main/java/com/mycompany/app/DataBase/ConnectionPostgreSQL.java
+++ src/main/java/com/mycompany/app/DataBase/ConnectionPostgreSQL.java
@@ -1,10 +1,12 @@
 package com.mycompany.app.DataBase;
 
+import org.apache.log4j.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class ConnectionPostgreSQL {
-
+    
+    private Logger logger = LogManager.getLogger(ConnectionPostgreSQL.class);
     private static Connection connection;
     private final static String URL = "jdbc:postgresql://localhost:5432/java_db";
     private final static String USER = "test";
@@ -16,12 +18,14 @@ public class ConnectionPostgreSQL {
     public static Connection getConnection() {
         return connection;
     }
-
+            
     public void init() {
         try {
             Class.forName("org.postgresql.Driver");
             ConnectionPostgreSQL.connection = DriverManager.getConnection(URL, USER, PASS);
+            logger.debug("Connection created:");
         } catch (Exception e) {
+            logger.debug("Can't connect to DB");
             e.printStackTrace();
         }
     }
